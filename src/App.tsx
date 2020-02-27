8e28e2f2beb386bfee3130b3afebb036601897ab
wanglifa <wanglifa1995@gmail.com>
2020-02-27 12:00:51 +0800

    引入react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import Header from "./common/header";
 import {Provider} from "react-redux";
 import store from "./store";
+import { BrowserRouter, Route } from 'react-router-dom'
 
 function App() {
   return (
     <Provider store={store}>
-      <Header/>
+      <div>
+        <Header/>
+        <BrowserRouter>
+          <div>
+            <Route path={"/"} render={() => <div>home</div>} exact></Route>
+            <Route path={"/detail"} render={() => <div>detail</div>} exact></Route>
+          </div>
+        </BrowserRouter>
+      </div>
     </Provider>
   );
 }
